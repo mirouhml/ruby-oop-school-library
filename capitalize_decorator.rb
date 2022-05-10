c3c4be8c4c52a650acfbecbaf6a83d56ee2cf7bc
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-10 09:12:27 +0100

    Fix linters errors.

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,11 +1,7 @@
 require_relative './base_decorator'
 
 class CapitalizeDecorator < BaseDecorator
-  def initialize(nameable)
-    super
-  end
-
   def correct_name
-    @nameable.correct_name.capitalize()
+    @nameable.correct_name.capitalize
   end
 end
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,10 +1,6 @@
 require_relative './base_decorator'
 
 class TrimmerDecorator < BaseDecorator
-  def initialize(nameable)
-    super
-  end
-  
   def correct_name
     name = @nameable.correct_name
     name.length > 10 ? name[0..9] : name
