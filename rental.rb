2828d7ad5855563ee1009796dd1a8c53742ab12f
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-10 16:51:56 +0100

    Add class variable to store rentals.

--- rental.rb
+++ rental.rb
@@ -2,9 +2,13 @@ class Rental
   attr_accessor :date
   attr_reader :book, :person
 
+  # rubocop:disable Style/ClassVars
+  @@rentals = []
+  # rubocop:enable Style/ClassVars
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
+    @@rentals.push(self)
   end
 end
