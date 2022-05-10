6ed06db3f8df88d790672ad8d91da9e634400c4c
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-10 09:02:32 +0100

    Add required tests.

--- main.rb
+++ main.rb
@@ -1,15 +1,10 @@
-require_relative './student'
-require_relative './teacher'
+require_relative './person'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
 
-person = Student.new(17, 'A', 'Ammar', parent_permission: false)
-
-puts(person.can_use_services?)
-puts(person.name)
-puts(person.name = ('Mirou'))
-puts(person.name)
-puts(person.age = (18))
-puts(person.can_use_services?)
-puts(person.play_hooky)
-
-teacher = Teacher.new(32, 'A', 'Ammar', parent_permission: true)
-puts(teacher.can_use_services?)
+person = Person.new(22, 'maximilianus')
+puts(person.correct_name)
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts(capitalizedPerson.correct_name)
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts(capitalizedTrimmedPerson.correct_name)
