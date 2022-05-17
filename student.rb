6eda2a723db4587f0f66e0e5a35a92e411df72e7
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-17 08:41:52 +0100

    Add store_students and restore_students.

--- student.rb
+++ student.rb
@@ -7,7 +7,6 @@ class Student < Person
   def initialize(age, classroom = 'Default', name = 'unknown', parent_permission: true)
     super age, name, parent_permission: parent_permission
     @classroom = Classroom.new(classroom)
-
     check = -> { @classroom.students.push(self) unless @classroom.students.include?(self) }
     check.call
   end
@@ -15,5 +14,25 @@ class Student < Person
   def classroom=(classroom)
     @classroom = Classroom.new(classroom)
     check.call
+  end
+
+  def self.store_students
+    students = {}
+    @@people.each_with_index do |p, index|
+      students[index] = { :name => p.name, :age => p.age, :parent_permission => p.parent_permission, :classroom => p.classroom.label } if p.is_a?(Student)
+    end
+    students = students.to_json
+    File.open("data/students.json","w") do |f|
+      f.write(students)
+    end
+    students
+  end
+
+  def self.restore_students
+    if File.exist?("data/students.json") 
+      file = File.open('data/students.json','r')
+      file_json = JSON.parse(file.read)
+      file_json.each_key { |key| new(file_json[key]['age'], file_json[key]['classroom'], file_json[key]['name'], parent_permission: file_json[key]['parent_permission']) }
+    end
   end
 end
